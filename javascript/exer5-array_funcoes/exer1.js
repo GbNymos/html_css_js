b59cac16a83a6d0d907c6e8cea8e16a1da3f5c0c
Gabriele oliveira <131557956+GbNymos@users.noreply.github.com>
2024-01-04 19:34:50 -0300

    fim do curso em video de js

---
+++ javascript/exer5-array_funcoes/exer1.js
@@ -0,0 +1,10 @@
+let num=[1,2,6,8]
+num.sort()
+num.push(7)
+console.log(num)
+
+let nomes=["gabi","tomaz","grazi","pill","lau"]
+
+for(let pos=0;pos<nomes.length;pos++){
+    console.log(nomes[pos],pos)
+}
---
+++ javascript/exer5-array_funcoes/objeto.js
@@ -0,0 +1,8 @@
+let amigos ={nome:"gabi", peso:50,engorda(){
+    console.log("engordou")
+
+} }
+
+amigos.engorda(5)
+console.log(amigos.nome)
+console.log(amigos.peso)
